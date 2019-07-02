caabcc6d7ff2f655e84de844f4134e624b484e0d
Rodrigo Tobar <rtobar@icrar.org>
2019-07-02 17:07:30 +0800

    Initial commit
    This first commit already contains a working version of what is needed
    by YAN-75. It depends on the latest version of daliuge, which has all
    the bits and pieces necessary for proper code reuse by other entities in
    a more simplified manner.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

---
+++ README.rst
@@ -0,0 +1,5 @@
+daliuge-pbc
+===========
+
+This package implements a Processing Block Controller
+based on the DALiuGE execution framework.
---
+++ test.py
@@ -0,0 +1,48 @@
+#
+#    ICRAR - International Centre for Radio Astronomy Research
+#    (c) UWA - The University of Western Australia, 2019
+#    Copyright by UWA (in the framework of the ICRAR)
+#    All rights reserved
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#    MA 02111-1307  USA
+#
+import unittest
+
+from dlg import testutils
+import dlg_pbc
+
+
+class ProcessingBlock(object):
+    pass
+
+
+class SimpleTest(unittest.TestCase, testutils.ManagerStarter):
+    def test_run_processing_block(self):
+
+        processing_block = ProcessingBlock()
+        processing_block.id = 'abc'
+        processing_block.workflow_id = 'workflow_id'
+        processing_block.workflow_version = '0.1'
+        processing_block.workflow_parameters = {}
+
+        statuses = []
+
+        def callback(status):
+            statuses.append(status)
+
+        with self.start_nm_in_thread(), self.start_dim_in_thread():
+            dlg_pbc.run_processing_block(processing_block, callback, zero_cost_run=True)
+        self.assertListEqual(['preparing', 'running', 'finished'], statuses)
